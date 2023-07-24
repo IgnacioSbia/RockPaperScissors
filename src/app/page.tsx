ed431d6cdc8df7cfaaa42136059133968de3f917
Ignacio <ignaciosabiajes@gmail.com>
2023-07-24 11:44:01 -0300

    minor imrpoves to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 'use client';
 import React, { useEffect, useState } from 'react'
 import Page from "./rules/page";
-
+import rock from '../../public/icon-rock.svg';
+import scissors from '../../public/icon-scissors.svg';
+import paper from '../../public/icon-paper.svg';
+import Image from 'next/image';
 
 export default function Home() {
   const [showModal, setShowModal] = useState(false);
@@ -16,6 +19,14 @@ export default function Home() {
       <div className='rockPaperScissorsPageModal'>
         {showModal ? <Page setModal={setShowModal}/> : ''}
       </div>
+      {/*Playground*/}
+      <section className='rockPaperScissorsPlayground'>
+        <aside className='rockPaperScissorsPaperRock'>
+          <button className='rockPaperScissorsPaper'><Image src={paper} width={38} alt='paper' /></button>
+          <button className='rockPaperScissorsScissors'><Image src={scissors}  width={38} alt='scissors' /></button>
+        </aside>
+        <button className='rockPaperScissorsRock'><Image src={rock} width={38} alt='rock' /></button>
+      </section>
       <aside className='rockPaperScissorsPageModalButton'>
        {showModal ? '' : <button onClick={()=>setShowModal(!showModal)} className='rockPaperScissorsModalButton'>Rules</button>}
       </aside>
