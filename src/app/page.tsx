de77c448d459d00c7ffe23301a2d50a9167eb14a
Ignacio <ignaciosabiajes@gmail.com>
2023-07-28 11:32:59 -0300

    almost done 95%

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,9 +10,10 @@ export default function Home() {
   const [showModal, setShowModal] = useState(false);
   const [winner, setWinner] = useState('');
   const [playerChoice, setPlayerChoice] = useState('');
-  const [computerChoice, setComputerChoice] = useState<any>('')
+  const [computerChoice, setComputerChoice] = useState('')
   const [playerScore, setPlayerScore] = useState<any>(0)
   const options = ['Rock','Scissors','Paper']
+  
 
   const computer = (min:number,max:number)=>{
     return Math.floor(Math.random() * (max - min ) +min)
@@ -27,28 +28,51 @@ export default function Home() {
       setWinner('Computer')
     }
   }
- 
-  console.log(options[computer(0, options.length)])
+  console.log(winner)
 
 
   return (
     <>
     <main className='rockPaperScissorsMainPage'>
       <section className='rockPaperScissorsPageTitleWithScore'>
-        <h1>Rock</h1>
-        <h1>Paper</h1>
-        <h1>Scissors</h1>
+        <div className='rockPaperScissorsPageTitle'>
+          <h1>Rock</h1>
+          <h1>Paper</h1>
+          <h1>Scissors</h1>
+        </div>  
+        <div className='rockPaperScissorsPageScore'>
+          <h1 className='rockPaperScissorsPageScoreTitle'>Score</h1>
+          <h1 className='rockPaperScissorsPageScoreScore'>{playerScore && playerScore>=0 ? playerScore : '0'}</h1>
+        </div>
       </section>
       <div className='rockPaperScissorsPageModal'>
         {showModal ? <Page setModal={setShowModal}/> : ''}
       </div>
+
+      
+        
+        {playerChoice && computerChoice ?
+        <div>
+          <aside>
+            <button className={`rockPaperScissors${playerChoice}`}><Image src={`/${playerChoice.toLowerCase}`} width={38} alt={`${playerChoice}`} height={38}/></button>
+            <h2>You picked</h2>
+          </aside>
+          <aside>
+          <button className={`rockPaperScissors${computerChoice}`}><Image src={`/${computerChoice.toLowerCase}`} width={38} alt={`${computerChoice}` }height={38}/></button>
+          <h2>The house picked</h2>
+          </aside>
+        </div>
+
+         :<></>}
+    
+
       {/*Playground*/}
       <section className='rockPaperScissorsPlayground'>
         <aside className='rockPaperScissorsPaperRock'>
-          <button className='rockPaperScissorsPaper'><Image src={paper} width={38} alt='paper' /></button>
-          <button className='rockPaperScissorsScissors'><Image src={scissors}  width={38} alt='scissors' /></button>
+          <button className='rockPaperScissorsPaper' onClick={()=>game('Paper')}><Image src={paper} width={38} alt='paper' /></button>
+          <button className='rockPaperScissorsScissors' onClick={()=>game('Scissors')}><Image src={scissors}  width={38} alt='scissors' /></button>
         </aside>
-        <button className='rockPaperScissorsRock'><Image src={rock} width={38} alt='rock' /></button>
+        <button className='rockPaperScissorsRock' onClick={()=>game('Scissors')}><Image src={rock} width={38} alt='rock' /></button>
       </section>
       <aside className='rockPaperScissorsPageModalButton'>
        {showModal ? '' : <button onClick={()=>setShowModal(!showModal)} className='rockPaperScissorsModalButton'>Rules</button>}
